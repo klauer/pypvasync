534e968a97e7b7823190843367a8477e3ad5e7c1
K Lauer <klauer@bnl.gov>
2016-02-03 11:04:21 -0500

    util clean

--- epics/utils.py
+++ epics/utils.py
@@ -4,32 +4,28 @@ String and data utils, where implementation differs between Python 2 & 3
 import os
 import sys
 from platform import architecture
-import sys
+
 
 if sys.version_info[0] < 3:
     raise ImportError(" Python version 3 required")
 
-EPICS_STR_ENCODING = 'ASCII'
-EPICS_STR_ENCODING = 'latin_1'
 NULLCHAR_2 = '\x00'
-NULLCHAR   = b'\x00'
+NULLCHAR = b'\x00'
 
-def s2b(st1):
-    'string to byte conversion'
+
+def STR2BYTES(st1):
+    '''string to byte conversion'''
     if isinstance(st1, bytes):
         return st1
-    return bytes(st1, EPICS_STR_ENCODING)
+    return bytes(st1, 'latin-1')
 
-def b2s(st1):
-    'byte to string conversion'
-    if isinstance(st1, str):
-        return st1
-    elif isinstance(st1, bytes):
-        return str(st1, EPICS_STR_ENCODING)
-    else:
-        return str(st1)
 
-STR2BYTES, BYTES2STR = s2b, b2s
+def BYTES2STR(st1):
+    '''byte to string conversion'''
+    if isinstance(st1, bytes):
+        return str(st1, 'latin-1')
+    return str(st1)
+
 
 def strjoin(sep, seq):
     "join string sequence with a separator"
@@ -38,7 +34,7 @@ def strjoin(sep, seq):
     if len(seq) == 0:
         seq = ''
     elif isinstance(seq[0], bytes):
-        tmp =[]
+        tmp = []
         for i in seq:
             if i == NULLCHAR:
                 break
@@ -46,14 +42,17 @@ def strjoin(sep, seq):
         seq = tmp
     return sep.join(seq)
 
+
 def is_string(s):
     return isinstance(s, str)
+
 
 def is_string_or_bytes(s):
     return isinstance(s, str) or isinstance(s, bytes)
 
+
 def ascii_string(s):
-    return bytes(s, EPICS_STR_ENCODING)
+    return s.encode('latin-1')
 
 
 PY64_WINDOWS = (os.name == 'nt' and architecture()[0].startswith('64'))
