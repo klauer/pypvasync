1bc903c60fd20a498473acbe100f032cd6b35921
K Lauer <klauer@bnl.gov>
2016-02-03 11:04:21 -0500

    remove utils2

--- epics/utils.py
+++ epics/utils.py
@@ -5,14 +5,7 @@ import os
 import sys
 from copy import deepcopy
 from platform import architecture
-
-
-PY_MAJOR, PY_MINOR = sys.version_info[:2]
-
-if PY_MAJOR >= 3:
-    from . import utils3 as utils_mod
-else:
-    from . import utils2 as utils_mod
+from . import utils3 as utils_mod
 
 STR2BYTES = utils_mod.STR2BYTES
 BYTES2STR = utils_mod.BYTES2STR
@@ -22,11 +15,7 @@ strjoin = utils_mod.strjoin
 is_string = utils_mod.is_string
 is_string_or_bytes = utils_mod.is_string_or_bytes
 ascii_string = utils_mod.ascii_string
-
 memcopy = deepcopy
-if PY_MAJOR == 2 and PY_MINOR == 5:
-    def memcopy(a):
-        return a
 
 PY64_WINDOWS = (os.name == 'nt' and architecture()[0].startswith('64'))
 PY_MAJOR, PY_MINOR = sys.version_info[:2]
