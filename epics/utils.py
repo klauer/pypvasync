bd83a21173eea051ea6c54bf783ca5307f1db5fb
K Lauer <klauer@bnl.gov>
2016-02-05 17:55:26 -0500

    nuke all string-related utils

--- epics/utils.py
+++ epics/utils.py
@@ -1,27 +1,8 @@
-"""
-String and data utils, where implementation differs between Python 2 & 3
-"""
 import os
 import sys
 from platform import architecture
 
-
 if sys.version_info[0] < 3:
-    raise ImportError(" Python version 3 required")
-
-NULLCHAR_2 = '\x00'
-NULLCHAR = b'\x00'
-
-
-def STR2BYTES(st1):
-    '''string to byte conversion'''
-    if isinstance(st1, bytes):
-        return st1
-    return bytes(st1, 'latin-1')
-
-
-def ascii_string(s):
-    return s.encode('latin-1')
-
+    raise ImportError("Python version 3 required")
 
 PY64_WINDOWS = (os.name == 'nt' and architecture()[0].startswith('64'))
