c73de44d50ba16798b29511a0c5e8dd1c5e9a4d1
K Lauer <klauer@bnl.gov>
2016-06-22 17:41:32 -0400

    Add simple synchronous test

---
+++ tests/test_sync.py
@@ -0,0 +1,42 @@
+import pytest
+import logging
+import functools
+from pvasync.sync import (caget, caput, blocking_mode)
+
+from . import pvnames
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def setup_module(module):
+    blocking_mode()
+
+
+def no_simulator_updates(fcn):
+    '''Context manager which pauses and resumes simulator PV updating'''
+    @functools.wraps(fcn)
+    def inner(*args, **kwargs):
+        try:
+            logger.debug('Pausing updating of simulator PVs')
+            print('* Pausing updating of simulator PVs')
+            caput(pvnames.pause_pv, 1)
+            return fcn(*args, **kwargs)
+        finally:
+            logger.debug('Resuming updating of simulator PVs')
+            print('* Resuming updating of simulator PVs')
+            caput(pvnames.pause_pv, 0)
+
+    return inner
+
+
+def test_caget():
+    value = caget(pvnames.double_pv)
+    assert value is not None
+
+
+@no_simulator_updates
+def test_caput():
+    caput(pvnames.enum_pv, 'Stop')
+    assert caget(pvnames.enum_pv) == 0
